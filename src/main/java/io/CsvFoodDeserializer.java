caa117bee76bc1e76b6912e4f26c3c2713995550
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2025-01-20 19:05:47 +0000

    Feature: Functionality for CsvFoodDeserializer

--- src/main/java/io/CsvFoodDeserializer.java
+++ src/main/java/io/CsvFoodDeserializer.java
@@ -2,6 +2,8 @@ package io;
 
 import model.Food;
 
+import static io.CsvFoodHeader.getFieldIndex;
+
 public class CsvFoodDeserializer implements FoodDeserializer {
     @Override
     public Food deserialize(String line) {
@@ -9,6 +11,17 @@ public class CsvFoodDeserializer implements FoodDeserializer {
     }
 
     private Food deserialize(String[] fields) {
+        return new Food(fields[getFieldIndex("food")],
+                parseDouble(fields[getFieldIndex("calories")]),
+                parseDouble(fields[getFieldIndex("protein")]),
+                parseDouble(fields[getFieldIndex("total_fat")]),
+                parseDouble(fields[getFieldIndex("carbohydrates")])
+        );
+    }
+
+    private Double parseDouble(String value) {
+        if(value.equals("NA")) return 0.0;
+        return Double.parseDouble(value);
 
     }
 }
